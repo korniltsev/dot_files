6a80ad0afa061371ada7ced0d2af6ae9ce58f936
Anatoly Korniltsev <korniltsev.anatoly@gmail.com>
2017-09-09 22:35:13 +0300

    Update generate_rtc_cmake.py

--- generate_rtc_cmake.py
+++ generate_rtc_cmake.py
@@ -1,5 +1,6 @@
 # very dumb script to find all .c/.h files and create a dumb CMakeLists.txt to import in CLion
 import os
+src_dir="src"
 excludes = [
   'webrtc/src/webrtc/sdk/objc/',
   'webrtc/src/webrtc/examples'
@@ -22,22 +23,72 @@ set(SOURCE_FILES
 
 """
 footer = """
-        webrtc/src/webrtc/pc/peerconnectionfactory_unittest.cc
-        webrtc/src/webrtc/pc/peerconnectioninterface_unittest.cc
 )
 
-include_directories(webrtc/src)
-include_directories(webrtc/src/third_party/googletest/src/googlemock/include)
-include_directories(webrtc/src/third_party/googletest/src/googletest/include)
-include_directories(webrtc/src/third_party/android_tools/ndk/platforms/android-22/arch-arm/usr/include)
+include_directories(%s)
+include_directories(%s/third_party/googletest/src/googlemock/include)
+include_directories(%s/third_party/googletest/src/googletest/include)
+include_directories(%s/third_party/android_tools/ndk/platforms/android-22/arch-arm/usr/include)
 add_definitions(-DWEBRTC_POSIX)
 add_definitions(-DGTEST_RELATIVE_PATH)
 add_definitions(-DWEBRTC_ANDROID)
 
 add_executable(cpp ${SOURCE_FILES})
 """
+
+manifest = """<?xml version="1.0" encoding="utf-8"?>
+<manifest package="webrtc.project.stub">
+</manifest>
+"""
+
+build_gradle = """
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:2.3.2'
+    }
+}
+
+apply plugin: 'com.android.library'
+
+android {
+    compileSdkVersion 26
+    buildToolsVersion "26.0.1"
+    defaultConfig {
+
+        minSdkVersion 16
+        targetSdkVersion 26
+        externalNativeBuild {
+            cmake {
+                cppFlags ""
+            }
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path "CMakeLists.txt"
+        }
+    }
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+
+    androidTestCompile('com.android.support.test.espresso:espresso-core:2.2.2', {
+        exclude group: 'com.android.support', module: 'support-annotations'
+    })
+    testCompile 'junit:junit:4.12'
+}
+
+"""
+footer = footer % (src_dir, src_dir, src_dir, src_dir)
 print header
-for root, directory, fs  in os.walk('webrtc/src/webrtc'):
+for root, directory, fs  in os.walk(src_dir + '/webrtc'):
   for f in fs:
     if f.endswith('.c') or f.endswith('.h') or f.endswith('.cpp') or f.endswith('.cc'):
       skip = False
@@ -52,3 +103,11 @@ for root, directory, fs  in os.walk('webrtc/src/webrtc'):
       print "    " + root + "/" + f
 
 print footer
+
+manifest_file = open('AndroidManifest.xml', 'w')
+manifest_file.write(manifest)
+manifest_file.close()
+
+build_gradle_file = open('build.gradle', 'w')
+build_gradle_file.write(build_gradle)
+build_gradle_file.close()
